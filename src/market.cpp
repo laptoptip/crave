27053a20483960a730b9f3ecb90616da39789273
Amir <cryptovote@yandex.com>
2015-08-30 12:28:38 -0500

    Updated copyright for Sling...

--- src/market.cpp
+++ src/market.cpp
@@ -1,4 +1,5 @@
 // Copyright (c) 2015 The Crave Developer (niitassin)
+// Copyright (c) 2015 The Sling developers
 // Distributed under the MIT/X11 software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
